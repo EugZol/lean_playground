1096bb3f76ddff9694514aa8896f22ffdf6ed447
Eugene Zolotarev <eugzol@gmail.com>
2025-03-08 02:12:43 +0300

    Lattice formalization

---
+++ Playground/Lattice.lean
@@ -0,0 +1,135 @@
+-- https://en.wikipedia.org/wiki/Lattice_(order)
+
+import Mathlib.Tactic
+
+namespace Playground
+
+-- A partially ordered set (poset) (L,≤) is called a lattice if it is both
+-- a join- and a meet-semilattice, i.e. each two-element subset {a,b} ⊆ L has
+-- a join (i.e. least upper bound) and dually a meet (i.e. greatest lower bound).
+
+structure PartialOrderMagma :=
+  (carrier : Type)
+  (lte : carrier → carrier → Prop)
+
+class PartialOrder (m : PartialOrderMagma) :=
+  (rfl : ∀ a, m.lte a a)
+  (asymm : ∀ a b, m.lte a b → m.lte b a → a = b)
+  (trans : ∀ a b c, m.lte a b → m.lte b c → m.lte a c)
+
+class SetLattice (po : PartialOrder m) :=
+  (two_join : ∀ a b, ∃ c, m.lte a c ∧ m.lte b c ∧ (
+    ∀ another_c, m.lte a another_c ∧ m.lte b another_c → m.lte c another_c
+  ))
+  (two_meet : ∀ a b, ∃ c, m.lte c a ∧ m.lte c b ∧ (
+    ∀ another_c, m.lte another_c a ∧ m.lte another_c b → m.lte another_c c
+  ))
+
+section SetLatticeProperties
+
+variable {m : PartialOrderMagma} [po : PartialOrder m]
+
+-- Each semilattice is the dual of the other.
+
+def dual_partial_order_magma (m : PartialOrderMagma) : PartialOrderMagma :=
+  {
+    carrier := m.carrier,
+    lte := fun a b => m.lte b a
+  }
+
+def dual_partial_order (po : PartialOrder m) : PartialOrder (dual_partial_order_magma m) :=
+  {
+    rfl := po.rfl,
+    asymm := fun a b hab hba => symm (po.asymm b a hab hba),
+    trans := fun a b c hab hbc => po.trans c b a hbc hab
+  }
+
+def dual_set_lattice (sl : SetLattice po) : SetLattice (dual_partial_order po) :=
+  {
+    two_join := fun a b => by
+      have h := sl.two_meet b a
+      tauto
+    two_meet := fun a b => by
+      have h := sl.two_join b a
+      tauto
+  }
+
+-- It follows by an induction argument that every non-empty finite subset of a lattice
+-- has a least upper bound and a greatest lower bound.
+
+theorem all_join (sl : SetLattice po) (S : Finset m.carrier) (ne : S ≠ ∅) : ∃ c, ((∀ x ∈ S, m.lte x c) ∧ (
+  ∀ another_c, (∀ x ∈ S, m.lte x another_c) → m.lte c another_c
+)) := by
+  induction S using Finset.induction_on
+  case inst =>
+    exact Classical.typeDecidableEq m.carrier
+  case empty =>
+    trivial
+  case insert a S2 _ IH =>
+    by_cases h : S2 = ∅
+    . use a
+      rw [h]
+      constructor
+      . simp
+        exact PartialOrder.rfl a
+      . simp
+    . apply IH at h
+      obtain ⟨c, ⟨hc1, hc2⟩⟩ := h
+      simp
+      have h := sl.two_join a c
+      obtain ⟨a', ⟨ha1', ⟨ha2', ha3'⟩⟩⟩ := h
+      use a'
+      constructor
+      . constructor
+        . trivial
+        . intro a''
+          intro a''_in_s
+          exact PartialOrder.trans a'' c a' (hc1 a'' a''_in_s) ha2'
+      . intro x'
+        intro a_lte_x'
+        intro x''
+        specialize ha3' x'
+        apply ha3'
+        tauto
+
+theorem all_meet (sl : SetLattice po) (S : Finset m.carrier) (ne : S ≠ ∅) : ∃ c, ((∀ x ∈ S, m.lte c x) ∧ (
+  ∀ another_c, (∀ x ∈ S, m.lte another_c x) → m.lte another_c c
+)) := by
+  have h := @all_join (dual_partial_order_magma m) (dual_partial_order po)
+    (dual_set_lattice sl) S ne
+  tauto
+
+end SetLatticeProperties
+
+structure LatticeDoubleMagma :=
+  (carrier : Type)
+  (join : carrier → carrier → carrier)
+  (meet : carrier → carrier → carrier)
+
+class Lattice (m : LatticeDoubleMagma) :=
+  (join_assoc : ∀ a b c, m.join (m.join a b) c = m.join a (m.join b c))
+  (meet_assoc : ∀ a b c, m.meet (m.meet a b) c = m.meet a (m.meet b c))
+  (join_comm : ∀ a b, m.join a b = m.join b a)
+  (meet_comm : ∀ a b, m.meet a b = m.meet b a)
+  (join_meet_absorb : ∀ a b, m.join a (m.meet a b) = a)
+  (meet_join_absorb: ∀ a b, m.meet a (m.join a b) = a)
+
+section LatticeProperties
+
+variable {m : LatticeDoubleMagma}
+
+open Lattice
+
+theorem join_idempotency (l : Lattice m) : ∀ a, m.join a a = a := by
+  intro  a
+  nth_rw 2 [← meet_join_absorb a a]
+  rw [join_meet_absorb]
+
+theorem meet_idempotency (_ : Lattice m) : ∀ a, m.meet a a = a := by
+  intro a
+  nth_rw 2 [← join_meet_absorb a a]
+  rw [meet_join_absorb]
+
+end LatticeProperties
+
+end Playground
